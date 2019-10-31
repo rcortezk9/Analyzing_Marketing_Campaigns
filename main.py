72ccfabff6e00c524734d32c2965fa85cb6cd6e1
Rene Cortez <rcortezk9@gmail.com>
2019-10-31 18:25:08 -0500

    Setting up our data to visualize daily conversion

--- main.py
+++ main.py
@@ -145,3 +145,13 @@ subscribers = marketing[marketing['converted'] == True]\
 
 # Calculate the conversion rate for all languages
 daily_conversion_rate = subscribers/total
+
+# %%
+
+# Reset index to turn the results into a DataFrame
+daily_conversion_rate = pd.DataFrame(daily_conversion_rate.reset_index(drop=False))
+
+# Rename columns
+daily_conversion_rate.columns = ['date_served',
+                              'conversion_rate']
+
