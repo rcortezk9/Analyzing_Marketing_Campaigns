231e91df03b2b67326125b70f03ed0ff9a18e683
Rene Cortez <rcortezk9@gmail.com>
2019-10-31 18:22:53 -0500

    Cleaning up date Visualization

--- main.py
+++ main.py
@@ -1,16 +1,147 @@
+# %%
 import pandas as pd
+import numpy as np
 
 # Import marketing.csv
-marketing = pd.read_csv('marketing.csv')
+marketing = pd.read_csv('marketing.csv',
+                        parse_dates=['date_served',
+                                     'date_subscribed',
+                                     'date_canceled']
+                        )
 
 # Examining the data
-# print(marketing.head())
-
+print(marketing.head())
 # Print the statistics of all columns
-# print(marketing.describe())
-
+print(marketing.describe())
 # Check column data types and non-missing values
-# print(marketing.info())
-
+print(marketing.info())
 # Check the data type of is_retained
 print(marketing['is_retained'].dtype)
+
+# %%
+# Convert is_retained to a boolean
+marketing['is_retained'] = marketing['is_retained'].astype('bool')
+
+# Mapping for channels
+channel_dict = {"House Ads": 1, "Instagram": 2, "Facebook": 3, "Email": 4, "Push": 5}
+
+# Map the channel to a channel code
+marketing['channel_code'] = marketing['subscribing_channel'].map(channel_dict)
+
+# Add the new column is_correct_lang
+marketing['is_correct_lang'] = np.where(
+    marketing['language_preferred'] == marketing['language_displayed'],
+    'Yes', 'No'
+)
+
+# Add a DoW column
+marketing['DoW'] = marketing['date_subscribed'].dt.dayofweek
+
+# Group by date_served and count number of unique user_id's
+daily_users = marketing.groupby(['date_served'])['user_id'].nunique()
+
+
+# Print head of daily_users
+print(daily_users.head())
+
+# %%
+import matplotlib.pyplot as plt
+
+# Plot daily_subscribers
+daily_users.plot()
+
+# Include a title and y-axis label
+plt.title('Daily users')
+plt.ylabel('Number of users')
+
+# Rotate the x-axis labels by 45 degrees
+plt.xticks(rotation=45)
+
+# Display the plot
+plt.show()
+
+# %%
+
+# Conversion Rate
+# Calculate the number of people we marketed to
+total = marketing['user_id'].nunique()
+
+# Calculate the number of people who subscribed
+subscribers = marketing[marketing['converted']==True]['user_id'].nunique()
+
+# Calculate the conversion rate
+conversion_rate = subscribers/total
+
+print(round(conversion_rate*100, 2), "%")
+
+# %%
+
+# Isolate english speakers
+english_speakers = marketing[marketing['language_displayed'] == 'English']
+
+# Calculate the total number of English speaking users
+total = english_speakers['user_id'].nunique()
+
+# Calculate the number of English speakers who converted
+subscribers = english_speakers[english_speakers['converted'] == True]['user_id'].nunique()
+
+# Calculate conversion rate
+conversion_rate = subscribers/total
+print('English speaker conversion rate:', round(conversion_rate*100,2), '%')
+
+# %%
+
+# Group by language_displayed and count unique users
+total = marketing.groupby(['language_displayed'])\
+                                  ['user_id'].nunique()
+
+# Group by language_displayed and count unique conversions
+subscribers = marketing[marketing['converted'] == True]\
+                       .groupby(['language_displayed'])\
+                       ['user_id'].nunique()
+
+# Calculate the conversion rate for all languages
+language_conversion_rate = subscribers/total
+print(language_conversion_rate)
+
+# %%
+
+# Group by date_served and count unique users
+total = marketing.groupby(['date_served'])['user_id'].nunique()
+
+# Group by date_served and count unique converted users
+subscribers = marketing[marketing['converted'] == True]\
+                       .groupby(['date_served'])\
+                       ['user_id'].nunique()
+
+# Calculate the conversion rate per day
+daily_conversion_rate = subscribers/total
+print(daily_conversion_rate)
+
+# Plotting campaign results(I)
+# %%
+
+# Create a bar chart using language_conversion_rate DataFrame
+language_conversion_rate.plot(kind='bar')
+
+# Add a title and x and y-axis labels
+plt.title('Conversion rate by language\n', size = 16)
+plt.xlabel('Age group', size = 14)
+plt.ylabel('Conversion rate (%)', size = 14)
+
+# Display the plot
+plt.show()
+
+# %%
+
+# Group by date_served and count unique users
+total = marketing.groupby(['date_served'])['user_id']\
+                     .nunique()
+
+# Group by date_served and calculate subscribers
+subscribers = marketing[marketing['converted'] == True]\
+                         .groupby(['date_served'])\
+                         ['user_id'].nunique()
+
+# Calculate the conversion rate for all languages
+daily_conversion_rate = subscribers/total
